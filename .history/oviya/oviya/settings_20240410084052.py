862bfd2ba4b6d47071007411f4fa93dd45b33b3e
Your Name <you@example.com>
2024-04-10 08:56:12 +0530

    added

--- .history/oviya/oviya/settings_20240410084052.py
+++ .history/oviya/oviya/settings_20240410084052.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','coverapp'
+    'django.contrib.staticfiles','o'
 ]
 
 MIDDLEWARE = [
